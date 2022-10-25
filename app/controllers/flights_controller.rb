63890b9436a3d560535c2f7cfc920e634752789b
Micheal Waldrup <waldrupm@gmail.com>
2022-10-25 17:05:44 -0400

    add bullet, flight search screen 1 done

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,10 +6,10 @@ class FlightsController < ApplicationController
       query = Flight.where(nil)
       query = query.where(departure_airport_id: params[:from_airport_id]) if params[:from_airport_id].present?
       query = query.where(arrival_airport_id: params[:to_airport_id]) if params[:to_airport_id].present?
-      query = query.where("DATE(departure_datetime) = ?", params[:date]) if params[:date].present?
-      @flights = query
+      query = query.where("DATE(departure_time) = ?", params[:date]) if params[:date].present?
+      @flights = query.includes(:departure_airport, :arrival_airport)
    else
-    @flights = Flight.all
+    @flights = Flight.all.includes(:departure_airport, :arrival_airport)
    end
   end
 end
