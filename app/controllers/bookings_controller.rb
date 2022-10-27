e34d33007056ffb6ec6d5d56f917539acece439e
Micheal Waldrup <waldrupm@gmail.com>
2022-10-27 16:56:18 -0400

    add db migrations, setups models and associations, accept nested attributes, begin form build

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,7 @@
 class BookingsController < ApplicationController
   def new
-    
+    @flight = Flight.find(params[:flight_id])
+    @booking = @flight.bookings.build
+    params[:num_passengers].to_i.times { @booking.passengers.build }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_204710) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_202409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_204710) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -30,4 +37,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_204710) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.bigint "booking_id", null: false
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "passengers", "bookings"
 end
