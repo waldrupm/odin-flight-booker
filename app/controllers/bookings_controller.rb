891e5bee9dad746d5cafbef23b81d397c6ad73b8
Micheal Waldrup <waldrupm@gmail.com>
2022-10-28 18:14:21 -0400

    finish bookings, very basic styling

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,4 +4,25 @@ class BookingsController < ApplicationController
     @booking = @flight.bookings.build
     params[:num_passengers].to_i.times { @booking.passengers.build }
   end
+
+  def create
+    # create the new booking and the associated passengers
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render 'new', notice: 'Booking failed', status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+  
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
+  end
+  
 end
